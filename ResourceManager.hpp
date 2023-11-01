8d8bb0ec900424c69627a83954ab30434c7f9c63
gommike <98242804+gommike@users.noreply.github.com>
2023-11-01 17:04:20 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,55 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    Resource resource;
+
+public:
+    ResourceManager() : resource()
+    {
+        // Konstruktor domyślny klasy ResourceManager, inicjalizuje obiekt typu Resource
+    }
+
+    double get() { return resource.get(); }
+
+    // 5 dobrze zdefiniowanych metod specjalnych klasy ResourceManager
+    // Możesz dodać odpowiednie konstruktory kopiujące, przenoszące, destruktory, itp.
+
+    // Destruktor
+    ~ResourceManager()
+    {
+        // Destruktor klasy ResourceManager
+    }
+
+    // Konstruktor kopiujący
+    ResourceManager(const ResourceManager& other) : resource(other.resource)
+    {
+        // Implementacja konstruktora kopiującego
+    }
+
+    // Operator przypisania
+    ResourceManager& operator=(const ResourceManager& other)
+    {
+        if (this != &other) {
+            resource = other.resource;
+            // Implementacja operatora przypisania
+        }
+        return *this;
+    }
+
+    // Konstruktor przenoszący
+    ResourceManager(ResourceManager&& other) noexcept : resource(std::move(other.resource))
+    {
+        // Implementacja konstruktora przenoszącego
+    }
+
+    // Operator przypisania przenoszącego
+    ResourceManager& operator=(ResourceManager&& other) noexcept
+    {
+        if (this != &other) {
+            resource = std::move(other.resource);
+            // Implementacja operatora przypisania przenoszącego
+        }
+        return *this;
+    }
 };
