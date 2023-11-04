070592f39abb9282e634cc297fee65b2d98c1be3
gommike <98242804+gommike@users.noreply.github.com>
2023-11-04 20:46:13 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,35 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private: 
+	Resource *resource;
+
+public:
+     ResourceManager() : resource(new Resource()) {}
+
+	 double get() { return resource->get(); }
+
+	 ResourceManager(const ResourceManager &diff) : resource(new Resource(*diff.resource)) {}  // konstruktor kopiowania
+
+	 ResourceManager& operator=(const ResourceManager& diff)  //zdefiniowanie przypisujacego operatora kopiowania
+         {
+             if (this != &diff) {
+                delete resource;
+                 resource = new Resource(*diff.resource);
+             }
+             return *this;
+         }
+     ResourceManager(ResourceManager&& diff) : resource(std::move(diff.resource)) {}  //kontruktor przenoszenia
+
+     ResourceManager& operator=(ResourceManager&& diff)
+     {
+             if (this != &diff) {
+                 delete resource;
+                 resource = std::move(diff.resource);
+             }
+             return *this;
+     }
+
+      ~ResourceManager() { delete resource; } //destruktor
+
 };
